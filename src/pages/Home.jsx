ad52dd7b25a067e7a3b62cc1a5aba9a830eddc3f
Danilo <danilodevsp@gmail.com>
2025-10-23 23:23:32 -0300

    feat: home page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,19 @@
+import Banner from '../components/Banner'
+import FeaturedSection from '../components/FeaturedSection'
+import Hero from '../components/Hero'
+import Newslatter from '../components/Newslatter'
+import Testimonials from '../components/Testimonials'
+
+const Home = () => {
+  return (
+    <>
+      <Hero />
+      <FeaturedSection />
+      <Banner />
+      <Testimonials />
+      <Newslatter />
+    </>
+  )
+}
+
+export default Home

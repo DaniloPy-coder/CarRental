9b6810c62247498d9b686e5e94b9fa9eb6fe706f
Danilo <danilodevsp@gmail.com>
2025-10-25 19:33:47 -0300

    feat: add component spin loader

---
+++ src/components/Loader.jsx
@@ -0,0 +1,9 @@
+const Loader = () => {
+  return (
+    <div className="flex h-[80vh] items-center justify-center">
+      <div className="h-14 w-14 animate-spin rounded-full border-4 border-gray-300 border-t-primary"></div>
+    </div>
+  )
+}
+
+export default Loader
